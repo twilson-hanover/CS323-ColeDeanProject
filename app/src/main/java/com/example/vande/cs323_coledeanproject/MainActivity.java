c382a130c41437853d456f45226b4e6d2c47a0d1
deanmock <opworld10@gmail.com>
2018-05-23 11:56:41 -0400

    added action bar added menu added to strings file added some minor functionality to java file added icon to menu

--- app/src/main/java/com/example/vande/cs323_coledeanproject/MainActivity.java
+++ app/src/main/java/com/example/vande/cs323_coledeanproject/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.vande.cs323_coledeanproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends Activity {
 
@@ -10,4 +15,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    @Override
+    public boolean onCreateInfoMenu(Menu menu) {
+        //Inflate menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch(item.getItemId()) {
+//            case R.id.action_instruct:
+//                //code to run when the item is clicked
+//
+//        }
+//    }
+
+
 }
