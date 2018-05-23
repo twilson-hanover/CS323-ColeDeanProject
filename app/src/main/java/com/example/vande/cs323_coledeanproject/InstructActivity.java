9aec47f61c0f2b0a8b49435dd8f086cc897cdc9f
deanmock <opworld10@gmail.com>
2018-05-23 13:53:06 -0400

    added text to action bar's page

---
+++ app/src/main/java/com/example/vande/cs323_coledeanproject/InstructActivity.java
@@ -0,0 +1,14 @@
+package com.example.vande.cs323_coledeanproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class InstructActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruct);
+    }
+}
--- app/src/main/java/com/example/vande/cs323_coledeanproject/MainActivity.java
+++ app/src/main/java/com/example/vande/cs323_coledeanproject/MainActivity.java
@@ -2,10 +2,9 @@ package com.example.vande.cs323_coledeanproject;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
 
 
 public class MainActivity extends Activity {
@@ -16,21 +15,25 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-//    @Override
-    public boolean onCreateInfoMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         //Inflate menu
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch(item.getItemId()) {
-//            case R.id.action_instruct:
-//                //code to run when the item is clicked
-//
-//        }
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_instruct:
+                //code to run when the item is clicked
+                Intent intent = new Intent(this, InstructActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
 
+        }
+    }
 
 }
